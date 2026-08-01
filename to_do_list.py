f37e1d5dc08233ea35420bc69413a23b3af7a8b0
Dhananjay Gurjar <dhananjaygurjar20@gmail.com>
2026-08-01 23:16:10 +0530

    todolist

---
+++ to_do_list.py
@@ -0,0 +1,6 @@
+# A command-line app where you can add, view, delete,
+# and mark tasks as done — basically a mini task manager that runs in the terminal.
+list = []
+for i in range():
+    list.appennd(input("Enter Your task:"))
+    
