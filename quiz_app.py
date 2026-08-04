0312311ffe36f624db149a8670e9c62d32fd69e3
Dhananjay Gurjar <dhananjaygurjar20@gmail.com>
2026-08-04 22:06:07 +0530

    Update quiz_app.py

--- quiz_app.py
+++ quiz_app.py
@@ -9,3 +9,7 @@ questions= ["2+4", "If time is 12:00PM will it morning or afternoon", "4/2"]
  
 
 for i in range(0, len(questions)):
+     indexing=questions[i]
+    print(indexing)
+    ans1=input("Enter Your Answer:")
+     
