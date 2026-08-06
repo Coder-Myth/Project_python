8cb8f252c94b30793af5c1bac968bed60c460f09
Dhananjay Gurjar <dhananjaygurjar20@gmail.com>
2026-08-07 04:12:29 +0530

    final

---
+++ code_decode/index.py
@@ -0,0 +1,18 @@
+# excercise 4solution
+
+task = input("1. Code \n\n2. Decode\n\nENTER YOUR CHOICE>>>>>>>>>>>")
+
+if not task.isdigit():
+    raise ValueError("Invalid input")
+
+elif task.isdigit():
+    task = int(task)
+    try:
+        if task == 1:
+            pass
+        elif(task ==2):
+            print(
+                "1. word less than 3 words reversed\n\n 2. atleast or more than three words \n (a)fisrt word at the last and three random modules present at the end ans well as front "
+            )
+    except:
+        print("Enter 1 or 2 as your integer:")
--- pass_generator.py
+++ pass_generator.py
@@ -1,2 +1,27 @@
+       elif(task ==2):
+            print(
+                "1. word less than 3 words reversed\n\n 2. atleast or more than three words \n (a)fisrt word at the last and three random modules present at the end ans well as front "
+            )
+    except:
+        print("Enter 1 or 2 as your integer:")
+
+40
 
 
+
+if a >= 10:
+    raise ValueError("Invalid input \n Enter Your Number Below 10")
+
+
+b = input("Number b/w 5&9:")
+if b == "quit":
+    print("Yeahhh")
+elif not b.isdigit():
+    raise ValueError("Not An Integer")
+else:
+    b=int(b)
+    if (b > 5) and (b < 9):
+        print(b)
+    else:
+        raise ValueError
+    38
