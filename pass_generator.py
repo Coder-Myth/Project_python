c911fba55f0797286f48850a4269af9f39e864d1
Coder_Myth <dhananjaygurjar20@gmail.com>
2026-08-10 23:44:32 +0530

    Update pass_generator.py

--- pass_generator.py
+++ pass_generator.py
@@ -1,15 +1,4 @@
-       elif(task ==2):
-            print(
-                "1. word less than 3 words reversed\n\n 2. atleast or more than three words \n (a)fisrt word at the last and three random modules present at the end ans well as front "
-            )
-    except:
-        print("Enter 1 or 2 as your integer:")
  
-
-if a >= 10:
-    raise ValueError("Invalid input \n Enter Your Number Below 10")
-
-
 b = input("Number b/w 5&9:")
 if b == "quit":
     print("Yeahhh")
