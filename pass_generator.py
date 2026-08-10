f4ad937671d4a47f72234256949e1bf7e2f68e4b
Coder_Myth <dhananjaygurjar20@gmail.com>
2026-08-10 23:44:55 +0530

    Update pass_generator.py

--- pass_generator.py
+++ pass_generator.py
@@ -1,13 +1,6 @@
- 
-b = input("Number b/w 5&9:")
-if b == "quit":
-    print("Yeahhh")
-elif not b.isdigit():
-    raise ValueError("Not An Integer")
-else:
-    b=int(b)
     if (b > 5) and (b < 9):
         print(b)
     else:
         raise ValueError
     38
+#not created till now 
