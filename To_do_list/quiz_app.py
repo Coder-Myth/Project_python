a4e971480ad5c6316cc2b25765628358882047cf
Coder_Myth <dhananjaygurjar20@gmail.com>
2026-08-12 19:27:51 +0530

    Update quiz_app.py

--- To_do_list/quiz_app.py
+++ To_do_list/quiz_app.py
@@ -1,4 +1,4 @@
-# kbc : create a program that asks you questions and tells you marks accordingly
+# kbc : create a program that asks you questions and tells you marks 
 #rewards are given at the last 
 
 questions = ["2+4", "16*2", "4/2"]
