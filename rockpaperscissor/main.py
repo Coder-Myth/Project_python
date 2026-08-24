f95529d35064f54845c8d6888d89665a7b2272c2
Coder-Myth <dhananjaygurjar20@gmail.com>
2026-08-24 23:47:38 +0530

    Update main.py

--- rockpaperscissor/main.py
+++ rockpaperscissor/main.py
@@ -40,3 +40,13 @@ except:
     print("<----Restart the Game--->\t")
 
  
+
+# input      1  2   3
+# rock    1  D  W   L
+# scissor 2  L  D   W
+# paper   3  W  L   D
+
+
+# the cleaner way for this is to make logic instead of the using numbers just like b==r i.e. draw then the three case in which i win keep them in or and use else to get other outputs
+
+#  give your output to ai and then see what are the mistake if it could be the professional generate the code by youself first
