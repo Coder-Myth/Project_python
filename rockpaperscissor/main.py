d953f57a85fd738e147f205ab783ca7f3627cdec
Coder-Myth <dhananjaygurjar20@gmail.com>
2026-08-24 23:47:14 +0530

    Update main.py

--- rockpaperscissor/main.py
+++ rockpaperscissor/main.py
@@ -9,4 +9,16 @@ try:
             b = int(user_inp)
             if b == 1 or b == 2 or b == 3:
                 r = random.randint(1, 3)
+                                # for rock as input
+                if r == 1 and b == 1:
+                    print("Draw\n\n")
+                elif r == 1 and b == 2:
+                    print("Win\n\n")
+                elif r == 1 and b == 3:
+                    print("Lose\n\n")
+                # for scissor as input
+                if r == 2 and b == 1:
+                    print("Lose\n\n")
+                elif r == 2 and b == 2:
+                    print("Win\n\n")
  
