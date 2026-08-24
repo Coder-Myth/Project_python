fee0150e206339830f0954f88b4d41b38cbbb762
Coder-Myth <dhananjaygurjar20@gmail.com>
2026-08-24 23:47:02 +0530

    Update main.py

--- rockpaperscissor/main.py
+++ rockpaperscissor/main.py
@@ -1,4 +1,12 @@
 # rock paper scissor:
 import random
-
+try:
+    while True:
+        user_inp = input(
+            "Enter \n1 for Rock \t\n2 for scissor\t\n3 for paper\t\n4 for Exiting\t\nEnter Your Choice>>>>>>>>>\n"
+        )
+        if user_inp.isdigit():
+            b = int(user_inp)
+            if b == 1 or b == 2 or b == 3:
+                r = random.randint(1, 3)
  
